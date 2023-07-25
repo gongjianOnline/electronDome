0542fe15579a85cb2279e2f1aa4868fed6a433e2
李承斌 <15031617367@163.com>
2023-07-25 14:28:05 +0800

    electronVite脚手架初始化

---
+++ electronvite/src/renderer/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>this is hello electronVite {{ xxx }}</div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue"
+const xxx = ref("1111")
+</script>
+
+<style>
+
+
+</style>
